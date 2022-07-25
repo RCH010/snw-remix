e46e571796f7d9f63baced76636743845feb9c16
Raul <raulcastellanosh@hotmail.com>
2022-07-25 13:45:31 -0500

    Add validations with zod

--- app/components/PostForm/PostForm.tsx
+++ app/components/PostForm/PostForm.tsx
@@ -1,7 +1,6 @@
-import type { ComponentPropsWithoutRef } from 'react';
+import type {Props} from './types';
 
-function PostForm({method= 'post', ...props}: ComponentPropsWithoutRef<'form'>) {
-
+function PostForm({error, fields, method= 'post', ...props}: Props) {
 
   return (
     <form method={method} className='flex flex-col gap-4' {...props}>
@@ -13,8 +12,9 @@ function PostForm({method= 'post', ...props}: ComponentPropsWithoutRef<'form'>) 
           className='p-4'
           name='title'
           placeholder='Title of your post'
+          defaultValue={fields?.title}
         />
-        
+        {error?.fieldErrors?.title && <p className='text-red-500'>{error.fieldErrors.title}</p>}
       </div>
       <div className='flex flex-col'>
         <label htmlFor='body' className='mb-2 text-gray-600'>
@@ -24,9 +24,12 @@ function PostForm({method= 'post', ...props}: ComponentPropsWithoutRef<'form'>) 
           className='p-4'
           name='body'
           placeholder='Write something amazing'
-        />
+          defaultValue={fields?.body}
+          />
+        {error?.fieldErrors?.body && <p className='text-red-500'>{error.fieldErrors.body}</p>}
       </div>
       
+      {error?.formError && <p className='text-red-500'>{error.formError}</p>}
       <button
         type='submit'
         className='transition rounded text-blue-700 font-bold py-4 px-6 transparent hover:bg-gray-100'
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,23 +1,52 @@
 import type { ActionFunction, LoaderFunction} from '@remix-run/node';
 import { redirect } from '@remix-run/node';
 import { json } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
+import { useActionData, useLoaderData } from '@remix-run/react';
 
 import {createPost, getPosts} from '~/services/posts.server';
 import type {Post} from '~/services/posts.server';
 import { Post as PostComponent} from '~/components/Post';
 import { PostForm } from '~/components/PostForm';
+import { CreatePost } from '~/services/validations';
 
 type LoaderData = {
   posts: Post[]
 }
 
+type ActionData = {
+  error: {
+    formError: string[]
+    fieldErrors: {
+      title?: string[]
+      body?: string[]
+    }
+  }
+  fields: {
+    title?: string
+    body?: string
+  }
+}
+
+
+
 export const action: ActionFunction = async({request}) => {
   const form = await request.formData()
   const rawTitle = form.get('title')
   const rawBody = form.get('body')
+  const result = CreatePost.safeParse({title: rawTitle, body: rawBody})
 
-  const post = await createPost({title: rawTitle, body: rawBody})
+
+  if(!result.success) {
+    return json({
+      error: result.error.flatten(),
+      fields: {
+        title: rawTitle,
+        body: rawBody,
+      }
+    }, {status: 400})
+  }
+
+  await createPost({title: result.data.title ?? null, body: result.data.body})
 
   return redirect('/')
 }
@@ -32,11 +61,14 @@ export const loader: LoaderFunction = async () => {
 export default function Index() {
 
   const {posts} = useLoaderData<LoaderData>()
+  const formData = useActionData<ActionData>()
 
   return (
     <div className='flex flex-col items-center' >
       <PostForm 
         action='/?index'
+        error={formData?.error}
+        fields={formData?.fields}
       />
       <ul>
         {posts.map(post => (
